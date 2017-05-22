cbb1d45131b2b40dac5417707124a1291f2648da
Anna <anna.tarasenko@gmail.com>
2017-05-22 13:41:50 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The following functions work together to create a square invertible matrix
+## and make the inverse of the matrix available in the cache environment
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {            ## if user want to reset matrix 
+        x <<- y                     ## reassign "new" matrix to x 
+        m <<- NULL                  ## reinitialize m to NULL
+    }
+    get <- function() x
+    setInvmatrix <- function(InvMatrix) m <<- InvMatrix
+    getInvmatrix <- function() m
+    list(set = set, get = get,
+         setInvmatrix = setInvmatrix,
+         getInvmatrix = getInvmatrix)
 
 }
-
-
-## Write a short comment describing this function
+## cacheSolve calcluates the inverse of the matrix created in makeCacheMatrix
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    m <- x$getInvmatrix()              
+    if(!is.null(m)) {           
+        return(m)               
+    }
+    data <- x$get()             
+    m <- solve(data, ...)       
+    x$setInvmatrix(m)           
+    m                            
 }
+
